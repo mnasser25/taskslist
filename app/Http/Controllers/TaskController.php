abfbc85151fae87d973f708494976760aeef8024
mnasser25 <marahnasser6@gmail.com>
2022-04-23 09:59:16 +0300

    commit4

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,56 +3,51 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
-
+use App\Models\Task;
 class TaskController extends Controller {
 
     public function index(){
-       // $tasks = [ 'task_1' => 'Make ur assignment',
-       //            'task_2' => 'Check ur phone',
-       //            'task_3' => 'Watch a movie'];
-
-       //$tasks = DB :: table('tasks') ->where('name','like','%Task 1%')-> get(); //لو دعمل شرط للاستدعاء بستخدم  where 
-       //$tasks = DB :: table('tasks') ->where('created_at','2022-04-05')-> get();
-       $tasks = DB :: table('tasks')->orderBy('name', 'asc') -> get();
-
+        $tasks = Task :: all()->sortBy("name");
+        //dd($tasks);
         return view('tasks',compact('tasks'));
        
     }
 
     public function show($id){
-        $task = DB::table('tasks') -> find($id) ;
+        $task = Task :: find($id);
         return view('show',compact('task'));
     }
 //Insert task to database 
 
     public function store(Request $request){
 
-       DB::table ('tasks') -> insert(['name'=> $_POST['name'],'created_at'=> now(),'updated_at'=> now()]);
-  
+       $task = new Task(); //بقوس أو دون صحيح 
+       $task->name = $request->name;
+       $task-> created_at = now();
+       $task ->updated_at = now();
+       $task -> save();
+
         return redirect() -> back();
 
     }
     // لأنو بعثتو في الراوت بقدر أستقبلو 
     public function delete($id){
-        DB:: table('tasks') -> where ('id','=',$id)-> delete();
+        $task = Task :: find($id);
+        $task -> delete();
         return redirect() -> back();
          
     }
-
+  
       
     public function edit($id){
-        $tasks = DB::table('tasks')->orderBy('name', 'asc')->get();
-        $task = DB::table('tasks')->find($id);
+        $tasks = Task :: all()->sortBy("name");
+        $task = Task :: find($id);
 
         return view('/tasks', compact('task', 'tasks'));
     }
 
     public function update(Request $request, $id){
-        $task = DB::table('tasks')->where('id',$id)->update([
-            'name' => $request->name
-        ]);
-
+        $task = Task :: where('id',$id)->update(['name' => $request->name]);
         return redirect('/');
     }
     
